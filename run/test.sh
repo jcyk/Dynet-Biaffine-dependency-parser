7da3c7614d8dc24819b44fcc7c36a2233bbfc26c
jcyk <thisisjcykcd@gmail.com>
2017-11-18 23:02:15 +0800

    clean up

--- run/test.sh
+++
@@ -1,5 +0,0 @@
-python train_notag.py --dynet-gpu --dynet-seed --pretrained_embeddings_file ../../word2vec_emb --save_dir ../ckpt/word2vec
-
-python train_notag.py --dynet-gpu --dynet-seed --pretrained_embeddings_file ../../dep2vec_typed --save_dir ../ckpt/dep2vec_typed
-
-python train_notag.py --dynet-gpu --dynet-seed --pretrained_embeddings_file ../../dep2vec_untyped --save_dir ../ckpt/dep2vec_untyped
